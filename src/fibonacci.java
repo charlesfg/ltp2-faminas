527cd6ac0afad3e70811f833a1a85ad325cbbd39
danielgsilvasantos <danielgsilvasantos@gmail.com>
2014-02-27 13:04:57 -0300

    Exercicios Slide II - Ex3-Fibonacci e Ex4-Sequencial

---
+++ src/fibonacci.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero = 0,
+		    atual = 1,
+		    anterior = 0,
+		    proximo;
+		    
+		
+		Scanner sc = new Scanner(System.in);
+		
+		do 
+		{
+			System.out.println("Informe um número inteiro menor que 20: ");
+			numero = sc.nextInt();
+			
+		} while(numero >= 20);
+		
+		for (int i =0; i <= numero; i++)
+		{
+			proximo = anterior + atual;
+			anterior = atual;
+			atual = proximo;
+			
+			System.out.println("Fibonacci N-ésimo número: " + proximo);
+		}
+
+	}
+
+}
---
+++ src/ltp2/faminasbh/java/Sequencial.java
@@ -0,0 +1,41 @@
+package ltp2.faminasbh.java;
+import java.util.Scanner;
+
+public class Sequencial {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int x = 0,
+			y=1,
+			aux = 3,
+			proximo = 1;
+			
+		
+		do{
+			System.out.print("Entre com o N-ésimo número:");
+			x = sc.nextInt();	
+		} while(x>=20);
+		
+//=========================================================Processamento da Sequencia==========
+			
+			if(x==1)
+			{
+				System.out.print("1");
+				y=x;
+				y++;
+			}//Fim se (Caso o usuario digite 1, ele imprime "1" e pula o laço for)
+		
+			else
+			{	
+				System.out.print("1");//Imprime "1" e escreve oresto do laço
+				
+				for(y = y; y < x; y++)//Escreve o resto da sequencia.
+				{
+					proximo = proximo + aux; //4,9,
+					aux = aux + 2;//5,7 
+					System.out.print(" "+proximo);
+				}
+			}
+	}
+}
